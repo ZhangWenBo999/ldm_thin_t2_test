bab3d7a0e3a9fc1d3a71e9ea9c48572018464879
ZhangWenBo999 <131420945+ZhangWenBo999@users.noreply.github.com>
2024-06-08 20:59:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -538,7 +538,7 @@ if __name__ == "__main__":
         trainer_config = lightning_config.get("trainer", OmegaConf.create())
         # default to ddp
         trainer_config["accelerator"] = "ddp"
-        trainer_config['max_epochs'] = 7
+        trainer_config['max_epochs'] = 8
         for k in nondefault_trainer_args(opt):
             trainer_config[k] = getattr(opt, k)
         if not "gpus" in trainer_config:

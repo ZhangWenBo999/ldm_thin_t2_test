1e66dde1fa4a037914d18bd2ca73e2e71291837b
ZhangWenBo999 <131420945+ZhangWenBo999@users.noreply.github.com>
2024-06-11 09:17:31 +0800

    Update inpaint.py

--- scripts/inpaint.py
+++ scripts/inpaint.py
@@ -5,10 +5,10 @@ from tqdm import tqdm
 import numpy as np
 import torch
 
-# import sys
-# sys.path.append("/kaggle/working/ldm_thin_t2_test")
+import sys
+sys.path.append("/kaggle/working/ldm_thin_t2_test")
 
-from ..main import instantiate_from_config
+from main import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 
 def make_batch(image, mask, device):
